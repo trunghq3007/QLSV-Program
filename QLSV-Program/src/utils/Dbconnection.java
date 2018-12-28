55ce6940f7def4d7a7ab8c39e95ee29881eb8191
hqtrung <haquangtrungceo@gmail.com>
2018-12-28 10:56:38 +0700

    add utils class

---
+++ QLSV-Program/src/utils/Dbconnection.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package utils;
+
+/**
+ * Create by: HQTrung - CMC
+ * Create date: Dec 28, 2018
+ * Modifier: HQTrung
+ * Modified date: Dec 28, 2018
+ * Description: handle connect to DBMS
+ * Version 1.0
+ */
+public class Dbconnection {
+
+}
---
+++ QLSV-Program/src/view/ManageStudent.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package view;
+
+import utils.Constants;
+
+/**
+ * Create by: HQTrung - CMC
+ * Create date: Dec 28, 2018
+ * Modifier: HQTrung
+ * Modified date: Dec 28, 2018
+ * Description: ....
+ * Version 1.0
+ */
+public class ManageStudent {
+
+        /**
+         * Create by: HQTrung - CMC
+         * Create date: Dec 28, 2018
+         * Modifier: HQTrung
+         * Modified date: Dec 28, 2018
+         * Description: program manage student
+         * Version 1.0
+         * @param args
+         */
+        public static void main(String[] args) {
+                System.out.println(Constants.Student.STUDENT_CODE);
+        }
+
+}
