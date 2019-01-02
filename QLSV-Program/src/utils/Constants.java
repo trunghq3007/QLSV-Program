8cf6330bed88b546d987c07c6c11e98e6bd37d47
trinhyen <trinhvermouth@gmail.com>
2019-01-02 09:14:54 +0700

    Add Log4J

--- QLSV-Program/src/utils/Constants.java
+++ QLSV-Program/src/utils/Constants.java
@@ -17,7 +17,7 @@ public interface Constants {
         public static final String UPDATE = "update sinhvien set TenSV = ?, GioiTinh = ?, NgaySinh = ?, QueQuan = ?, MaLop = ? where MaSV = ?;";
         public static final String DELETE = "delete from sinhvien where MaSV = ?;";
         public static final String SEARCH = "select * from sinhvien where TenSV like ?;";
-        
+
     }
 
     interface Course {
@@ -45,8 +45,8 @@ public interface Constants {
         public static final String SEARCH = "select * from hedt where TenHeDT like ?;";
 
     }
-    
-    interface Subject{
+
+    interface Subject {
 
         public static final String SELECT_ALL = "select * from monhoc;";
         public static final String GET_BY_CODE = "select * from monhoc where MaMH = ?;";
@@ -54,20 +54,19 @@ public interface Constants {
         public static final String UPDATE_ALL = "update from monhoc set TenMH = ?, SoTrinh = ? where MaMH = ?;";
         public static final String DELETE_ALL = "delete from monhoc where MaMH = ?;";
         public static final String SEARCH = "select * from monhoc where TenMH like ?;";
-        
+
     }
-    
-    interface Mark{
+
+    interface Mark {
 
         String SELECT_ALL = "select * from diem;";
         String GET_BY_CODE = "select * from diem where MaSV = ?";
         String ADD_ALL = "INSERT INTO diem VALUES (?, ?, ?, ?, ?)";
         String UPDATE_ALL = "update diem set HocKy = ?, DiemLan1 = ?, DiemLan2 = ? where MaSV = ? and MaMH = ? ;";
-        
-        
+
     }
-    
-    interface LOP{
+
+    interface LOP {
 
         String SELECT_ALL = "select * from lop";
         String GET_BY_CODE = "select * from lop where MaLop = ?";
@@ -75,10 +74,10 @@ public interface Constants {
         String UPDATE_ALL = "update lop set TenLop = ?, MaKhoa = ?, MaHeDT = ?, MaKhoaHoc = ? where MaLop = ?;";
         String DELETE_ALL = "delete from lop where MaLop = ?";
         String SEARCH = "select * from lop where TenLop like ?;";
-        
+
     }
-    
-    interface Faculty{
+
+    interface Faculty {
 
         String SELECT_ALL = "select * from khoa;";
         String GET_BY_CODE = "select * from khoa where MaKhoa = ?;";
@@ -86,7 +85,7 @@ public interface Constants {
         String UPDATE = "update khoa set TenKhoa = ?, DiaChi = ?, Phone = ? where MaKhoa = ?";
         String DELETE = "delete from khoa where MaKhoa = ?;";
         String SEARCH = "select * from khoa where TenKhoa like ?;";
-        
+
     }
 
 }
